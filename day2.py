79ff2cf0bed197219b04abbc3b3a60e0f6b52ccf
Mohit Mahesh Vaswani <iam_mohitv@Mohits-MBP.lan>
2023-03-20 21:59:16 -0400

    Python day 6: datatypes

---
+++ day2.py
@@ -0,0 +1,24 @@
+#variables in python
+#author: MV
+
+
+a = 1
+b = True
+c = "Mohit"
+d = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# print(a + c) error
+
+# list is basically array of different items
+list1 = [1,2,3,4]
+print(list1)
+# tuples are same as list but it is immutable
+tuple1 = (1,2,3,4)
+print(tuple1)
+# dict is key value pair (map basically)
+dict1 = {"mv":1,"king":2}
+print(dict1)
