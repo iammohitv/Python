071c13e843195b2957754e495858c7091927a3e3
Mohit Mahesh Vaswani <iam_mohitv@Mohits-MBP.lan>
2023-03-20 21:23:49 -0400

    day2

--- day1.py
+++ day1.py
@@ -1,1 +1,8 @@
-print("Hello World")
+print("Hello World",10)
+
+# this is a comment
+print("MOhit \nmohit is python learner")
+
+print("I am mohit \"vaswani\" \n")
+
+print("This is","basic","string with sep ",sep="~",end="basis end statement\n")
