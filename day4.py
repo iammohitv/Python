7ba10a7b2a6232829c3018e7fd33f579640f8e7c
Mohit Mahesh Vaswani <iam_mohitv@Mohits-MBP.lan>
2023-03-20 22:22:36 -0400

    Inputs and basic datatypes

---
+++ day4.py
@@ -0,0 +1,17 @@
+# use this for ref: https://replit.com/@codewithharry/09-Day9-Typecasting-in-Python
+a = "59"
+b = "1"
+
+print(int(a)+int(b))
+
+#use int(var_name)
+
+
+#implicit typecasting c is automatically converted to float
+
+a = 1
+b = 2.0
+c = a + b
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ day5.py
@@ -0,0 +1,6 @@
+#taking user inputs
+a = int(input("Please enter a value: "))
+
+print(a)
+
+# if you don't define datatype it will take string
