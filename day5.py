ba6b094cbaa598bb3212dd7e02c747ec7dc3ee08
Mohit Mahesh Vaswani <iam_mohitv@Mohits-MBP.lan>
2023-03-22 21:53:00 -0400

    String intro

--- day5.py
+++ day5.py
@@ -4,3 +4,10 @@ a = int(input("Please enter a value: "))
 print(a)
 
 # if you don't define datatype it will take string
+
+b = input("b: ")
+c = input("c: ")
+print(b+c)
+# print(b-c) unsupported for string
+# print(b*c) unsupported for string
+# print(b/c) unsupported for string
---
+++ strings.py
@@ -0,0 +1,13 @@
+a = "mohit"
+b = '''this is multiline string
+
+and it works'''
+print("king, " + a)
+print(b)
+print(a[0]) #0 indexing in strings
+
+
+#looping in string
+
+for i in b:
+    print(i)
