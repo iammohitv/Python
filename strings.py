cf7578dc473ba3508c11ed8694efae36ef52f6b8
Mohit Mahesh Vaswani <iam_mohitv@Mohits-MBP.lan>
2023-03-22 23:56:42 -0400

    Strings

--- strings.py
+++ strings.py
@@ -11,3 +11,9 @@ print(a[0]) #0 indexing in strings
 
 for i in b:
     print(i)
+
+#string slicing
+print(a[0:4]) #prints 0123 index of a
+print(a[:4])
+print(a[1:4])
+print(a[0:-3]) #prints 0 till len(a)-3 5-3 basically 2 hence 01
